87e20be17bc352d6f0ad486f977f1f14c74a0549
gensing <tohvv15h4@naver.com>
2023-04-23 22:49:14 +0900

    service 사용으로 변경

--- src/main/kotlin/com/example/demo/member/controller/MemberController.kt
+++ src/main/kotlin/com/example/demo/member/controller/MemberController.kt
@@ -2,17 +2,44 @@ package com.example.demo.member.controller
 
 import com.example.demo.member.model.data.Member
 import com.example.demo.member.service.MemberService
+import kotlinx.coroutines.flow.Flow
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
+@RequestMapping("/members")
 @RestController
-class MemberController(private val memberService: MemberService) {
+class MemberController(
+        private val memberService: MemberService
+) {
 
-    @GetMapping("/members/{id}")
-    suspend fun getUserById(@PathVariable id: Long): ResponseEntity<Member> {
-        val user = memberService.findOne(id)
-        return if (user != null) ResponseEntity.ok(user) else ResponseEntity.notFound().build()
+    @PostMapping("")
+    suspend fun create(@RequestBody member: Member): ResponseEntity<Member> {
+        return memberService.create(member)
+                .let { ResponseEntity.ok().build() }
+    }
+
+    @GetMapping("")
+    suspend fun findAll(@PathVariable id: Long): ResponseEntity<Flow<Member>> {
+        return memberService.findAll()
+                .let { ResponseEntity.ok(it) }
+    }
+
+    @GetMapping("/{id}")
+    suspend fun findOne(@PathVariable id: Long): ResponseEntity<Member> {
+        return memberService.findOne(id)
+                ?.let { ResponseEntity.ok(it) }
+                ?: ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    suspend fun update(@PathVariable id: Long, @RequestBody member: Member): ResponseEntity<Any> {
+        return memberService.update(id, member)
+                .let { ResponseEntity.ok().build() }
+    }
+
+    @DeleteMapping("/{id}")
+    suspend fun delete(@PathVariable id: Long): ResponseEntity<Any> {
+        return memberService.delete(id)
+                .let { ResponseEntity.ok().build() }
     }
 }
--- src/main/kotlin/com/example/demo/member/handler/MemberHandler.kt
+++ src/main/kotlin/com/example/demo/member/handler/MemberHandler.kt
@@ -1,40 +1,43 @@
 package com.example.demo.member.handler
 
 import com.example.demo.member.model.data.Member
-import com.example.demo.member.repository.MemberRepositoryCoroutine
+import com.example.demo.member.service.MemberService
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.server.*
-import java.lang.RuntimeException
 
 
 @Configuration
 class MemberHandler(
-        private val repository: MemberRepositoryCoroutine
+        private val memberService: MemberService
 ) {
 
+    suspend fun create(request: ServerRequest): ServerResponse {
+        val member = request.awaitBodyOrNull<Member>() ?: throw RuntimeException()
+        return memberService.create(member)
+                .let { ServerResponse.ok().buildAndAwait() }
+    }
+
     suspend fun findAll(request: ServerRequest): ServerResponse {
-        return ServerResponse.ok().json().bodyValueAndAwait(repository.findAll())
+        return ServerResponse.ok().json().bodyValueAndAwait(memberService.findAll())
     }
 
     suspend fun findOne(request: ServerRequest): ServerResponse {
         val id = request.pathVariable("id").toLong()
-        return repository.findById(id)?.let { ServerResponse.ok().json().bodyValueAndAwait(it) }
+        return memberService.findOne(id)
+                ?.let { ServerResponse.ok().json().bodyValueAndAwait(it) }
                 ?: ServerResponse.notFound().buildAndAwait();
-    }
-
-    suspend fun create(request: ServerRequest): ServerResponse {
-        val member = request.awaitBodyOrNull<Member>() ?: throw RuntimeException()
-        return repository.save(member).let { ServerResponse.ok().buildAndAwait() }
     }
 
     suspend fun update(request: ServerRequest): ServerResponse {
         val id = request.pathVariable("id").toLong()
         val member = request.awaitBodyOrNull<Member>() ?: throw RuntimeException()
-        return repository.save(Member(id, member.username)).let { ServerResponse.ok().buildAndAwait() }
+        return memberService.update(id, member)
+                .let { ServerResponse.ok().buildAndAwait() }
     }
 
     suspend fun delete(request: ServerRequest): ServerResponse {
         val id = request.pathVariable("id").toLong()
-        return repository.deleteById(id).let { ServerResponse.ok().buildAndAwait() }
+        return memberService.delete(id)
+                .let { ServerResponse.ok().buildAndAwait() }
     }
 }
