a0bed63fb457bf689c856f7f932ebb56a4c76028
gensing <tohvv15h4@naver.com>
2023-04-23 22:49:05 +0900

    service crud 로직 추가

--- src/main/kotlin/com/example/demo/member/service/MemberService.kt
+++ src/main/kotlin/com/example/demo/member/service/MemberService.kt
@@ -2,13 +2,30 @@ package com.example.demo.member.service
 
 import com.example.demo.member.model.data.Member
 import com.example.demo.member.repository.MemberRepositoryCoroutine
+import kotlinx.coroutines.flow.Flow
 import org.springframework.stereotype.Service
 
 @Service
-class MemberService(val repository: MemberRepositoryCoroutine) {
+class MemberService(private val repository: MemberRepositoryCoroutine) {
+
+    fun findAll(): Flow<Member> {
+        return repository.findAll()
+    }
 
     suspend fun findOne(id: Long): Member? {
         return repository.findById(id)
+    }
+
+    suspend fun create(member: Member): Long {
+        return repository.save(member).id
+    }
+
+    suspend fun update(id: Long, member: Member) {
+        repository.save(Member(id, member.username))
+    }
+
+    suspend fun delete(id: Long) {
+        repository.deleteById(id)
     }
 
 }
